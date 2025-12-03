1fcdff868d52fff9e4ef0097af8cfebbffea2cbc
Artur <geghamyan.artur.dev@gmail.com>
2025-12-03 16:54:30 +0400

    Fix app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
